596afddd57fa7b88e54cc07c66f66b417b3e7675
amdycoder <amdyhm0@gmail.com>
2025-01-23 03:12:37 +0100

    ajoute des composants

---
+++ src/app/electeur/electeur.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-electeur',
+  imports: [],
+  templateUrl: './electeur.component.html',
+  styleUrl: './electeur.component.css'
+})
+export class ElecteurComponent {
+
+}
