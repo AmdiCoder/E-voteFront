e4512de143b32ecfc392651b930bc685257f8455
MpG <mandiayegaye0@gmail.com>
2025-01-23 20:26:08 +0000

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,1 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  standalone: true,  // Si tu veux utiliser un composant standalone
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
-export class HomeComponent {  // Assure-toi que la classe est nommée 'HomeComponent'
-  title = 'eVote';
-}
 
